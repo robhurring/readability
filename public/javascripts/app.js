d8a8cc9812ab6fc1f92465e21d11c05179cde5cd
robhurring <robhurring@gmail.com>
2013-12-09 11:38:22 -0500

    add ladda

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,7 +5,6 @@ var app = angular.module('readability', [
 
 app.config(function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('/');
-
   $stateProvider
     .state('home', {
       url: '/',
@@ -13,6 +12,27 @@ app.config(function($stateProvider, $urlRouterProvider){
       controller: 'HomeCtrl'
     })
 })
+
+app.directive('uiLadda', [function(){
+  return {
+    link: function postLink(scope, element, attrs) {
+      var ladda = Ladda.create(element[0]);
+
+      scope.$watch(attrs.uiLadda, function(newVal, oldVal){
+        if (angular.isNumber(oldVal)) {
+          if (angular.isNumber(newVal)) {
+              ladda.setProgress(newVal);
+          } else {
+              newVal && ladda.setProgress(0) || ladda.stop();
+          }
+        } else {
+          newVal && ladda.start() || ladda.stop();
+        }
+      });
+    }
+  };
+}]);
+
 
 app.factory('TextStatisticsSvc', function(){
   return {
